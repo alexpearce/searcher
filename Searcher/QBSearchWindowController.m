268c4ef34f4697403143a5e56eee77db71746c19
Alex Pearce <alex@alexpearce.me>
2013-12-20 15:20:51 +0000

    Change title column to view-based cell.

--- Searcher/QBSearchWindowController.m
+++ Searcher/QBSearchWindowController.m
@@ -86,39 +86,54 @@
 
 - (CGFloat)tableView:(NSTableView *)tableView heightOfRow:(NSInteger)row
 {
-  CGFloat itemRowHeight = ITEM_ROW_HEIGHT + ITEM_ROW_PADDING;
   if (tableView == _groupTableView) {
     NSInteger numTracksForProperty = [_tracksController propertyAtIndexTrackCount:row];
     if (numTracksForProperty < GROUP_ROW_HEIGHT) {
       numTracksForProperty = GROUP_ROW_HEIGHT;
     }
-    CGFloat rowHeight = numTracksForProperty*itemRowHeight;
-    return rowHeight;
+    return numTracksForProperty*(ITEM_ROW_HEIGHT + ITEM_ROW_PADDING);
   } else {
-    return itemRowHeight;
+    return ITEM_ROW_HEIGHT;
   }
 }
 
 - (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
 {
-  NSString *columnIdentifier = [tableColumn identifier];
-  if (tableView == _groupTableView) {
-    // Let's make sure it's the group column, otherwise return nil
-    if ([columnIdentifier isEqualToString:@"GroupColumn"]) {
-      QBTableCellView *cellView = [tableView makeViewWithIdentifier:columnIdentifier owner:self];
-      NSDictionary *group = [_tracksController groupAtIndex:row];
-      [cellView.imageView setImage:group[@"image"]];
-      [cellView.textField setStringValue:group[@"artist"]];
-      [cellView.albumField setStringValue:group[@"album"]];
-      return cellView;
+  NSString *columnIdentifier = tableColumn.identifier;
+  if ([columnIdentifier isEqualToString:@"group"]) {
+    QBTableCellView *cellView = [tableView makeViewWithIdentifier:columnIdentifier owner:self];
+    NSDictionary *group = [_tracksController groupAtIndex:row];
+    cellView.imageView.image = group[@"image"];
+    cellView.textField.stringValue = group[@"artist"];
+    cellView.albumField.stringValue = group[@"album"];
+    return cellView;
+  } else if ([columnIdentifier isEqualToString:@"title"]) {
+    // No need for views in dummy cells
+    if ([self rowInTableView:tableView IsDummy:row]) {
+      return nil;
     }
-    return nil;
-  } else {
-    if ([columnIdentifier isEqualToString:@"title"]) {
-      NSTableCellView *cellView = [tableView makeViewWithIdentifier:columnIdentifier owner:self];
-      [cellView.textField setStringValue:@"yoyoyo"];
-      return cellView;
+    NSString *cellViewStringValue;
+    NSUInteger groupItemsCount;
+    NSUInteger dummyRowsCount = 0;
+    NSUInteger totalRowsCount = 0;
+    NSUInteger trackNum;
+    for (NSDictionary *group in [_tracksController groups]) {
+      groupItemsCount = [group[@"count"] unsignedIntegerValue];
+      totalRowsCount += groupItemsCount;
+      if (totalRowsCount > row) {
+        trackNum = row - dummyRowsCount;
+        QBTrack *track = [_tracksController trackAtIndex:trackNum];
+        cellViewStringValue = [track valueForKey:[tableColumn identifier]];
+        break;
+      }
+      if (groupItemsCount < GROUP_ROW_HEIGHT) {
+        totalRowsCount += GROUP_ROW_HEIGHT - groupItemsCount;
+        dummyRowsCount += GROUP_ROW_HEIGHT - groupItemsCount;
+      }
     }
+    NSTableCellView *cellView = [tableView makeViewWithIdentifier:columnIdentifier owner:self];
+    cellView.textField.stringValue = cellViewStringValue;
+    return cellView;
   }
   return nil;
 }
@@ -148,39 +163,11 @@
   }
 }
 
-- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
-{
-  if (tableView == _groupTableView) {
-    return [_tracksController groupAtIndex:row];
-  } else {
-    NSUInteger groupItemsCount;
-    NSUInteger dummyRowsCount = 0;
-    NSUInteger totalRowsCount = 0;
-    NSUInteger trackNum;
-    if ([self rowInTableView:tableView IsDummy:row]) {
-      return @"";
-    }
-    for (NSDictionary *group in [_tracksController groups]) {
-      groupItemsCount = [group[@"count"] unsignedIntegerValue];
-      totalRowsCount += groupItemsCount;
-      if (totalRowsCount > row) {
-        trackNum = row - dummyRowsCount;
-        QBTrack *track = [_tracksController trackAtIndex:trackNum];
-        return [track valueForKey:[tableColumn identifier]];
-      }
-      if (groupItemsCount < GROUP_ROW_HEIGHT) {
-        totalRowsCount += GROUP_ROW_HEIGHT - groupItemsCount;
-        dummyRowsCount += GROUP_ROW_HEIGHT - groupItemsCount;
-      }
-    }
-  }
-  return @"";
-}
-
 #pragma mark - QBTableViewDataSource
 
 - (void)activateSelectedRow:(NSTableView *)tableView
 {
+  // TODO we need to subtract dummy rows from this
   [self activateAtIndex:[tableView selectedRow]];
 }
 
