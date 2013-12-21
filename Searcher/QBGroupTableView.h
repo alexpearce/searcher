507a2b5f4274ffe3251c7696636026624427800b
Alex Pearce <alex@alexpearce.me>
2013-12-21 22:52:11 +0000

    Remake window to fix odd responder chain problem.

---
+++ Searcher/QBGroupTableView.h
@@ -0,0 +1,16 @@
+//
+//  QBGroupTableView.h
+//  Searcher
+//
+//  Created by Alex Pearce on 21/12/2013.
+//  Copyright (c) 2013 Alex Pearce. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+/**
+ * An NSTableView which does not accept first responder.
+ */
+@interface QBGroupTableView : NSTableView
+
+@end
---
+++ Searcher/QBGroupTableView.m
@@ -0,0 +1,18 @@
+//
+//  QBGroupTableView.m
+//  Searcher
+//
+//  Created by Alex Pearce on 21/12/2013.
+//  Copyright (c) 2013 Alex Pearce. All rights reserved.
+//
+
+#import "QBGroupTableView.h"
+
+@implementation QBGroupTableView
+
+- (BOOL)acceptsFirstResponder
+{
+  return NO;
+}
+
+@end
