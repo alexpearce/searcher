4e7f18de43341f4a722e5eec2e3a5a7530cae5b2
Alex Pearce <alex@alexpearce.me>
2013-12-26 17:17:06 +0000

    Don't know why this hasn't been added..

---
+++ Searcher/QBiTunesController.h
@@ -0,0 +1,15 @@
+//
+//  QBITunesController.h
+//  Searcher
+//
+//  Created by Alex Pearce on 26/12/2013.
+//  Copyright (c) 2013 Alex Pearce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "QBMusicSource.h"
+
+@interface QBiTunesController : NSObject <QBMusicSource>
+
+@end
